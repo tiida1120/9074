fa7f8f566be2635b6dea7b186a1d95a93123dbda
git-osaku <osaku48@gmail.com>
2015-01-24 15:02:51 +0900

    ピンの追加、markers.xmlの追加

--- web/js/map/map.js
+++ web/js/map/map.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
 
 function apply_map() {
     var coordinates = {
-        latitude:135,
-        longitude:35.65
+        latitude:136.670213,
+        longitude:36.579160
+
     };
 
-    show_google_map('map-canvas', coordinates.longitude, coordinates.latitude, 5);
+    show_google_map('map-canvas', coordinates.longitude, coordinates.latitude, zoom, title);
 }
 
