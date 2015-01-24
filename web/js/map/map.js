87f90b9af5f40f82f98d6562062343c83adbbe76
Takashi Iida <iida@ipublishing.jp>
2015-01-24 23:36:24 +0900

    マーカー表示の実装&リソースを追加

--- web/js/map/map.js
+++ web/js/map/map.js
@@ -1,14 +1,42 @@
+var googleMap = null;
+
 $(document).ready(function(){
-    apply_map();
+    init_map();
 });
 
-function apply_map() {
-    var coordinates = {
-        latitude:136.670213,
-        longitude:36.579160
+function init_map() {
+    googleMap = null;
 
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(36.565842, 136.658941),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
 
-    show_google_map('map-canvas', coordinates.longitude, coordinates.latitude, zoom, title);
+    googleMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    load_spots();
 }
 
+function load_spots() {
+    $.ajax( {
+        url: '../../res/data/bearInfo.min.json',
+        dataType : 'json',
+        success: function(data) {
+            for (var i=0; i<data.length; i++) {
+                add_marker(data[i]);
+            }
+        },
+        error: function(data) {
+            // TODO:エラー時の処理
+        }
+    });
+}
+
+function add_marker(spot) {
+    var latlng = new google.maps.LatLng(spot.lat, spot.lng);
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: googleMap,
+    });
+}
