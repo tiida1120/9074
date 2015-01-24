a84f26e9cffb1fb0f2b880a0f721de107b42094c
Takashi Iida <iida@ipublishing.jp>
2015-01-25 01:30:15 +0900

    InfoWindow表示機能を追加

--- web/js/map/map.js
+++ web/js/map/map.js
@@ -1,9 +1,5 @@
 var googleMap = null;
 
-$(document).ready(function(){
-    init_map();
-});
-
 function init_map() {
     googleMap = null;
 
@@ -35,8 +31,28 @@ function load_spots() {
 
 function add_marker(spot) {
     var latlng = new google.maps.LatLng(spot.lat, spot.lng);
+
+    // TODO:レベルに応じたマーカーの表示
+
     var marker = new google.maps.Marker({
       position: latlng,
       map: googleMap,
     });
+
+    var infoContent = $('<div id="info-container" style="width:100%; height:100%;"><div id="info-date"></div><div id="info-place"></div><div id="info-detail"></div></div>');
+    infoContent.find('#info-date').text('日時：' + spot.date);
+    infoContent.find('#info-place').text('住所：' + spot.place);
+    infoContent.find('#info-detail').text('詳細：' + spot.detail);
+
+    var infowindow = new google.maps.InfoWindow({
+        content: infoContent.html()
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(googleMap, marker);
+    });
 }
+
+$(function(){
+    init_map();
+});
